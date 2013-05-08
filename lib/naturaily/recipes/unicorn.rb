734b0cd69cb3250ead30f60ff44cfa5739d444f8
Przemek Lusar <lluzak@gmail.com>
2013-05-08 10:06:22 +0200

    fix problem reloading unicorn using usr2 signal

--- lib/naturaily/recipes/unicorn.rb
+++ lib/naturaily/recipes/unicorn.rb
@@ -7,17 +7,17 @@ Capistrano::Configuration.instance.load do
   namespace :deploy do
     desc "Restart the master unicorn process"
     task :restart , :roles => :app do
-      run "/etc/init.d/unicorn_#{application} restart"
+      run "BUNDLE_GEMFILE=#{current_path}/Gemfile /etc/init.d/unicorn_#{application} restart"
     end
 
     desc "Start the master unicorn process"
     task :start , :roles => :app do
-      run "/etc/init.d/unicorn_#{application} start"
+      run "BUNDLE_GEMFILE=#{current_path}/Gemfile /etc/init.d/unicorn_#{application} start"
     end
 
     desc "Stop the master unicorn process"
     task :stop , :roles => :app do
-      run "/etc/init.d/unicorn_#{application} stop"
+      run "BUNDLE_GEMFILE=#{current_path}/Gemfile /etc/init.d/unicorn_#{application} stop"
     end
   end
 
